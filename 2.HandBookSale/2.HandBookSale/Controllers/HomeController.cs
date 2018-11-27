c01e39f7a62f16877d53712eaf4d3bb646777574
semihagursoy <eng.semihagursoy@gmail.com>
2018-11-27 23:53:44 +0300

    Gülcenin Kodları
    Gülcenin kodları eklendi

--- 2.HandBookSale/2.HandBookSale/Controllers/HomeController.cs
+++ 2.HandBookSale/2.HandBookSale/Controllers/HomeController.cs
@@ -7,24 +7,26 @@ using System.Web.Mvc;
 namespace _2.HandBookSale.Controllers
 {
     public class HomeController : Controller
-    {
-        public ActionResult Index()
         {
-            return View();
+            public ActionResult Index()
+            {
+                return View();
+            }
+            [Route("UyeOl")]
+
+            public ActionResult UyeOl()
+            {
+                return View();
+            }
+            [Route("UyeGirisi")]
+            public ActionResult UyeGirisi()
+            {
+                return View();
+            }
+            [Route("BizeUlasin")]
+            public ActionResult BizeUlasin()
+            {
+                return View();
+            }
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
 }
