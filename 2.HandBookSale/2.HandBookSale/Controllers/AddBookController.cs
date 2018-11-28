2a87c65d82e7a8f255678d6dbee0135cc4552a5d
semihagursoy <eng.semihagursoy@gmail.com>
2018-11-28 21:15:48 +0300

    database işleri
    entity framework ile database eklendi

---
+++ 2.HandBookSale/2.HandBookSale/Controllers/AddBookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2.HandBookSale.Controllers
+{
+    public class AddBookController : Controller
+    {
+        // GET: AddBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
